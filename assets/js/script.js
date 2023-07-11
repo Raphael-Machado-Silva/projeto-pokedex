bbb04a4995ef3a550f4a40c78efeda8f05b6ff2e
Raphael Machado Silva <rafael.machado.6996@gmail.com>
2023-07-11 10:52:57 -0300

    Update project-pokedex

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 const pokemonName = document.querySelector('.pokemon_name');
-
+const pokemonNumber = document.querySelector('.pokemon_number');
+const pokemonImage = document.querySelector('.pokemon_image');
 
 const fetchPokemon = async (pokemon) => { /*PEGAR OS DADOS DO POKEMON*/
  
@@ -12,6 +13,8 @@ const renderPokemon = async (pokemon) => { /*MOSTRAR OS DADOS (NOME)*/
     const data = await fetchPokemon(pokemon);
 
     pokemonName.innerHTML = data.name;
+    pokemonNumber.innerHTML = data.id;
+    pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
 }
 
-renderPokemon('25');
+renderPokemon('26');

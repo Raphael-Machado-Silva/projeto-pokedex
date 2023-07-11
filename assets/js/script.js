88ec8a1ec745177074e96d9c2d569b1719d3a1e0
Raphael Machado Silva <rafael.machado.6996@gmail.com>
2023-07-11 10:31:58 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,9 @@
 const fetchPokemon = async (pokemon) => {
  
     const APIResponse = await fetch(`https://pokeapi.co/api/v2/${pokemon}`);
-    console.log(APIResponse);
+    
+    const data =  await APIResponse.json();
+
+    return data;
 }
 
-fetchPokemon();

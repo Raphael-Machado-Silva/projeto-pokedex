0cac8ad28a81848fcb20e26144fecc92e2c0bd9f
Raphael Machado Silva <rafael.machado.6996@gmail.com>
2023-07-11 11:14:27 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,23 +5,27 @@ const form = document.querySelector('.form');
 const input = document.querySelector('.input_search');
 
 const fetchPokemon = async (pokemon) => { /*PEGAR OS DADOS DO POKEMON*/
- 
     const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
-    const data =  await APIResponse.json();
-    return data;
+
+    if(APIResponse.status === 200){
+            const data =  await APIResponse.json();
+            return data;
+    } else {}
 }
 
 const renderPokemon = async (pokemon) => { /*MOSTRAR OS DADOS (NOME, NÚMERO e GIF)*/
     const data = await fetchPokemon(pokemon);
 
-    pokemonName.innerHTML = data.name;
+    if(data){
+            pokemonName.innerHTML = data.name;
     pokemonNumber.innerHTML = data.id;
-    pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+    pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];    
+
+    input.value = '';
+    } else{}
 }
 
 form.addEventListener('submit', (event) => { /*MUDAR OS DADOS DE ACORDO COM O INPUT */
     event.preventDefault();
-
     renderPokemon(input.value.toLowerCase());
-    input.value = '';
 });

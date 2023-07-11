37e1ec2fe5f9d9181637e26349f2e69b9c346c3f
Raphael Machado Silva <rafael.machado.6996@gmail.com>
2023-07-11 10:44:32 -0300

    Update projeto-pokedex

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,17 @@
-const fetchPokemon = async (pokemon) => {
- 
-    const APIResponse = await fetch(`https://pokeapi.co/api/v2/${pokemon}`);
-    
-    const data =  await APIResponse.json();
+const pokemonName = document.querySelector('.pokemon_name');
 
+
+const fetchPokemon = async (pokemon) => { /*PEGAR OS DADOS DO POKEMON*/
+ 
+    const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+    const data =  await APIResponse.json();
     return data;
 }
 
+const renderPokemon = async (pokemon) => { /*MOSTRAR OS DADOS (NOME)*/
+    const data = await fetchPokemon(pokemon);
+
+    pokemonName.innerHTML = data.name;
+}
+
+renderPokemon('25');
